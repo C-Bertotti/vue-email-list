36de8c0efe47be1efe6428d0cc5d1178440e3a4e
Caterina Bertotti <bertotti4997@gmail.com>
2021-04-07 17:28:06 +0200

    axios

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
 var app = new Vue (
     {
-        el: '',
-        data: ,
-        mounted:
+        el: '#root',
+        data: {
+            email: 'ciao',
+            emailList: []
+        },
+        mounted: function() {
+            for ( var i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( (rndEmail) => {
+                    this.email = rndEmail.data.response;
+                });
+                console.log(this.email);
+            };
+        }
 
     }
 );
